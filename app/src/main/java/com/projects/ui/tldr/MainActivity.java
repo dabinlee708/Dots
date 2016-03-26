8ac968b8a7dd98061d0eb73db8412f0d760047fa
mhatib <muhd.hatib@gmail.com>
2016-03-26 15:22:14 +0800

    added loading page

--- app/src/main/java/com/projects/ui/tldr/MainActivity.java
+++ app/src/main/java/com/projects/ui/tldr/MainActivity.java
@@ -1,7 +1,11 @@
 package com.projects.ui.tldr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,loadingActivity.class));
+            }
+
+        });
     }
+
+
 }
