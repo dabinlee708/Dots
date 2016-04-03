1a608bb82e60281205257427ea8a782501bb3706
mhatib <muhd.hatib@gmail.com>
2016-04-03 13:32:05 +0800

    added auto load calendar activity from loading activity afer 3 secs

--- app/src/main/java/com/projects/ui/tldr/loadingActivity.java
+++ app/src/main/java/com/projects/ui/tldr/loadingActivity.java
@@ -1,5 +1,7 @@
 package com.projects.ui.tldr;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class loadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i=new Intent(loadingActivity.this,CalendarActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
     }
 }
