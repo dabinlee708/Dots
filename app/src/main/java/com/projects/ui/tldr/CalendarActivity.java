3178f1ee8ac7400eff67055974ce0df75ba5b00b
mhatib <muhd.hatib@gmail.com>
2016-04-07 00:47:01 +0800

    added swipe detection, had to disable scroll view

--- app/src/main/java/com/projects/ui/tldr/CalendarActivity.java
+++ app/src/main/java/com/projects/ui/tldr/CalendarActivity.java
@@ -2,10 +2,13 @@ package com.projects.ui.tldr;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,8 +17,9 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class CalendarActivity extends AppCompatActivity {
-
+public class CalendarActivity extends AppCompatActivity{
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +39,33 @@ public class CalendarActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+
+                    Intent myIntent = new Intent(CalendarActivity.this, EventActivity.class);
+                    myIntent.putExtra("T", "test");
+                    startActivity(myIntent);
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
     public void onClick(View v) {
         Log.d("mytag", "onClick:UI" + v.getId());
         TextView text = (TextView) findViewById(v.getId());
@@ -51,12 +82,16 @@ public class CalendarActivity extends AppCompatActivity {
         // set values for custom dialog components - text, image and button
         TextView title = (TextView) dialog.findViewById(R.id.title);
         title.setText(selection.getText());
+        TextView eventDetails = (TextView) dialog.findViewById(R.id.details);
+        eventDetails.setText("Need to dynamically add hardcoded details for the events based on the available choices.\nPerhaps choose a few to demo.");
         ImageView image = (ImageView) dialog.findViewById(R.id.logo);
         image.setImageResource(R.drawable.noon);
         ImageView image2 = (ImageView) dialog.findViewById(R.id.logo2);
         image2.setImageResource(R.drawable.money);
         ImageView image3 = (ImageView) dialog.findViewById(R.id.logo3);
-        image3.setImageResource(R.drawable.night);
+        image3.setImageResource(R.drawable.nomoney);
+        ImageView image4 = (ImageView) dialog.findViewById(R.id.logo4);
+        image4.setImageResource(R.drawable.nomoney);
 
         dialog.show();
 
@@ -84,20 +119,8 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
 
-//        TextView selection = (TextView) findViewById(R.id.selected);
-//        selection.setText(text.getText());
-//        Button btnDetails = (Button) findViewById(R.id.button);
-//        btnDetails.setVisibility(v.VISIBLE);
-//        btnDetails.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                Intent myIntent = new Intent(CalendarActivity.this, EventActivity.class);
-//                myIntent.putExtra("T", selection.getText());
-//                startActivity(myIntent);
-//            }
-//
-//        });
-        ScrollView scr = (ScrollView) findViewById(R.id.scrlayout);
-        scr.fullScroll(ScrollView.FOCUS_DOWN);
+//        ScrollView scr = (ScrollView) findViewById(R.id.scrlayout);
+//        scr.fullScroll(ScrollView.FOCUS_DOWN);
 
 
     }
