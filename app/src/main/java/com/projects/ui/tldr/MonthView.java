d912f293820147aec6138c55e79a24bc8e6c8c1f
dabin.lee@outlook.com <Chwb5278!>
2016-04-12 14:50:08 +0800

    Date collection for intent completed. Done in a toast. It will be sent with an intent later on to the calendar activity.

--- app/src/main/java/com/projects/ui/tldr/MonthView.java
+++ app/src/main/java/com/projects/ui/tldr/MonthView.java
@@ -1,13 +1,27 @@
 package com.projects.ui.tldr;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MonthView extends AppCompatActivity {
+    CalendarView cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_view);
+        cal = (CalendarView) findViewById(R.id.calendarView1);
+
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getBaseContext(),"Selected Date is\n\n" + dayOfMonth + " / " + month + " / " + year, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
 }
